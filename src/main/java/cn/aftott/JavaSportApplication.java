0b77570499c23b38f93fd5fadd9da0246850b753
aftott <1578649337@qq.com>
2022-03-21 12:57:10 +0800

    Initial commit

---
+++ src/main/java/cn/aftott/JavaSportApplication.java
@@ -0,0 +1,13 @@
+package cn.aftott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/aftott/JavaSportApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.aftott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
